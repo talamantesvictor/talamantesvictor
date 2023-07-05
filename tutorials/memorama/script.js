fd2071d94a685b30d52728a5f421665927fc682f
Victor Talamantes <hola@victortalamantes.com>
2023-07-04 18:36:31 -0600

    Add card image and total attempts to memorama

--- tutorials/memorama/script.js
+++ tutorials/memorama/script.js
@@ -1,19 +1,25 @@
 const totalCards = 12;
+const availableCards = ['A', 'K', 'Q', 'J'];
 let cards = [];
 let selectedCards = [];
 let valuesUsed = [];
 let currentMove = 0;
+let currentAttempts = 0;
 
 let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
 
 function activate(e) {
    if (currentMove < 2) {
-      e.target.classList.add('active');
-
-      if (!selectedCards[0] || selectedCards[0] !== e.target) {
+      
+      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active') ) {
+         e.target.classList.add('active');
          selectedCards.push(e.target);
 
          if (++currentMove == 2) {
+
+            currentAttempts++;
+            document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
+
             if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
                selectedCards = [];
                currentMove = 0;
@@ -42,12 +48,20 @@ function randomValue() {
    }
 }
 
+function getFaceValue(value) {
+   let rtn = value;
+   if (value < availableCards.length) {
+      rtn = availableCards[value];
+   }
+   return rtn;
+}
+
 for (let i=0; i < totalCards; i++) {
    let div = document.createElement('div');
    div.innerHTML = cardTemplate;
    cards.push(div);
    document.querySelector('#game').append(cards[i]);
    randomValue();
-   cards[i].querySelectorAll('.face')[0].innerHTML = valuesUsed[i];
+   cards[i].querySelectorAll('.face')[0].innerHTML = getFaceValue(valuesUsed[i]);
    cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
 }
